7cee0c87c05b6dc9e9136c7b3a6ac2ac1fb3e894
andrewdreamer13 <isaevandrey9@gmail.com>
2025-02-03 20:11:33 +0200

    wrote a card flip mechanism in the shop slider

--- src/scripts/_shop.js
+++ src/scripts/_shop.js
@@ -19,13 +19,13 @@ const shopSlider = new Swiper(".shop__slider", {
       // shadow: true,
       // translate: ["-120%", 0, -500],
       opacity: 0,
-      scale: 0
+      scale: 0,
     },
     next: {
       // shadow: true,
       // translate: ["120%", 0, -500],
       opacity: 0,
-      scale: 0
+      scale: 0,
     },
   },
   centeredSlides: true,
@@ -44,9 +44,14 @@ const shopModel = document.querySelector("#shop-model");
 const shopRating = document.querySelector("#shop-rating");
 const shopPrice = document.querySelector("#shop-price");
 
+const shopCards = document.querySelectorAll(".shop__card");
+const frontBtn = document.querySelector("#front-btn");
+const backBtn = document.querySelector("#back-btn");
+
+let currentIndex;
+
 shopSlider.on("slideChange", () => {
-  const currentIndex = shopSlider.realIndex;
-  console.log(currentIndex);
+  currentIndex = shopSlider.realIndex;
 
   surfs.forEach((surf) => {
     if (surf.id == currentIndex) {
@@ -54,7 +59,42 @@ shopSlider.on("slideChange", () => {
       shopPrice.innerHTML = `$${surf.price}<sup>99</sup>`;
       const activeRatingWidth = surf.rating / 0.05;
       shopRating.style.width = `${activeRatingWidth}%`;
-      console.log(activeRatingWidth);
     }
   });
+
+  updateBtnsState();
+
+  frontBtn.addEventListener("click", function () {
+    shopCards[currentIndex].classList.remove("shop__card-rotated");
+    this.classList.add("shop__card-btn-active");
+    backBtn.classList.remove("shop__card-btn-active");
+  });
+
+  backBtn.addEventListener("click", function () {
+    shopCards[currentIndex].classList.add("shop__card-rotated");
+    this.classList.add("shop__card-btn-active");
+    frontBtn.classList.remove("shop__card-btn-active");
+  });
 });
+
+frontBtn.addEventListener("click", function () {
+  shopCards[0].classList.remove("shop__card-rotated");
+  this.classList.add("shop__card-btn-active");
+  backBtn.classList.remove("shop__card-btn-active");
+});
+
+backBtn.addEventListener("click", function () {
+  shopCards[0].classList.add("shop__card-rotated");
+  this.classList.add("shop__card-btn-active");
+  frontBtn.classList.remove("shop__card-btn-active");
+});
+
+function updateBtnsState() {
+  if (shopCards[currentIndex].classList.contains("shop__card-rotated")) {
+    backBtn.classList.add("shop__card-btn-active");
+    frontBtn.classList.remove("shop__card-btn-active");
+  } else {
+    backBtn.classList.remove("shop__card-btn-active");
+    frontBtn.classList.add("shop__card-btn-active");
+  }
+}
